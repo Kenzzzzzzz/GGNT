28090ae397a62c9c167454460413a384d1048330
Kenzzzzzzz <115093686+Kenzzzzzzz@users.noreply.github.com>
2022-10-31 11:18:03 +0300

    projectN1

---
+++ ggnt/WindowsFormsApp1/DataBaseLib/Commands/Table1Commands.cs
@@ -0,0 +1,53 @@
+﻿using DataBaseLib.Access;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseLib.Commands
+{
+    internal class Table1Commands : ITableEditorCommand
+    {
+        // ЕСЛИ нужна другая БД, просто меняем AccessDataBaseController
+        // на нужную, например SQLiteDataBaseController
+        // в следующей строке
+        private AccessDataBaseController controller = new AccessDataBaseController();
+
+        public void Select(string[] args)
+        {
+
+        }
+
+        public void Insert(string[] args)
+        {            
+            string query = $"INSERT INTO [Ученики] " +
+                    $"([Номер ученика], [ФИО], [Класс], [Классух], [Тариф], [День учебы])" +
+                    $"VALUES ('{args[0]}', '{args[1]}', '{args[2]}', '{args[3]}', '{args[4]}', '{args[5]}')";
+            controller.ExecuteCommand(query);
+        }
+
+        public void Update(string[] args)
+        {
+            // дома
+
+            /*
+             string query = $"UPDATE [Категории] SET [Категория товара] = '{type}', " +
+                    $"[Скидка %] = {discount} " +
+                    $"WHERE [ID] = {id}";
+             */
+        }
+
+        public void Delete(string[] args)
+        {
+            // дома
+
+            /*
+             string query = $"DELETE FROM [Категории] " +
+                    $"WHERE [ID] = {id}";
+             */
+        }
+
+
+    }
+}
---
+++ ggnt/WindowsFormsApp1/DataBaseLib/DataBaseCommadsManager.cs
@@ -0,0 +1,60 @@
+﻿using DataBaseLib.Access;
+using DataBaseLib.Commands;
+using System.Data;
+
+namespace DataBaseLib;
+
+/// <summary>
+/// Выполнение команд пользователя
+/// </summary>
+public class DataBaseCommadsManager
+{
+
+
+    public DataTable GetDataTable(string tableName)
+    {
+        BaseCommands command = new BaseCommands();        
+        return command.GetDataTable(tableName);
+    }
+
+    public void Insert(string[] args, string tableName)
+    {
+        ITableEditorCommand command;
+        switch(tableName)
+        {
+            case "Ученики": command = new Table1Commands();
+                break;
+            default: throw new Exception("Ошибка!");
+        }        
+        command.Insert(args);
+    }
+
+    public void Update(string[] args,string tabeleName)
+    {
+        ITableEditorCommand command;
+        switch(tabeleName)
+        {
+            case "Ученики" : command = new Table1Commands();
+                break;
+            default: throw new Exception("ошибка!");
+        }
+        command.Update(args);
+    }
+
+    public void Delete(string[] args, string tabelName)
+    {
+        ITableEditorCommand command;
+        switch(tabelName)
+        {
+            case "Ученики": command = new Table1Commands();
+                break;
+            default: throw new Exception("Ошибка!");
+        }
+        command.Delete(args);
+    }
+    // Домашнее задание
+    // 1. Дописать логику методов Update, Delete
+    // 2. Продумать структуру БД по вариантам
+    // 3. Использовать Access или SQLite
+
+}
